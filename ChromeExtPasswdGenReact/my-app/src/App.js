e141bbcc22ed8768813c31ea9aac8a2d9ab20eb9
DanielH260654 <260654@student.calderdale.ac.uk>
2023-01-13 16:05:51 +0000

    added the front end of pass gen in react version

--- ChromeExtPasswdGenReact/my-app/src/App.js
+++ ChromeExtPasswdGenReact/my-app/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+  //document.getElementById("reload").addEventListener('click', function () {
+  //  var text = document.getElementById('textField');
+  //  const result = makeid(13)
+  //  text.value = "";
+  //  text.value += result;
+  //});
+//
+  //document.querySelector("#copy").addEventListener("click", copy);
+//
+  //function copy() {
+  //    var copyText = document.querySelector("#textField");
+  //    copyText.select(); document.execCommand("copy");
+  //}
+//
+  //function makeid(length) {
+  //    var result = '';
+  //    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!+=*&^%$£"@#.~<>,.;:[]{}()_-?';
+  //    var charactersLength = characters.length;
+  //    for ( var i = 0; i < length; i++ ) {
+  //        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  //    }
+  //    return result;
+  //}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h3>Generate a password</h3>
+        <input type="text" value="" id="textField" readonly />
+        <p>  </p>
+        <input id="reload" type="button" value="reload" />
+        <p>  </p>
+        <button  id="copy">Copy</button>
+        <br />
     </div>
   );
 }
